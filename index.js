6d50b0ce2c9ac47f68ec9302daf5663c3af61784
linder <achie_31@yahoo.com>
2024-08-18 14:44:09 +0300

    worked on the api and updateder the foot

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 function refreshWeather(response){
  let temperatureElement = document.querySelector("#temperature-degree");
  let temperature = response.data.temperature.current;      
- let cityElement = document.querySelector("#city");         
- cityElement.innerHTML = response.data.city;
+ let cityElement = document.querySelector("#city");  
+ let descriptionElement = document.querySelector("#description");
+ let humidityElement = document.querySelector("#humidity");
+ let windElement = document.querySelector("#wind");
+ let timeElement = document.querySelector("#time");
+ let date = new Date(response.data.time * 1000);
 
+ timeElement.innerHTML =  formatDate(date);
+ windElement.innerHTML = `${response.data.wind.speed}km/h`;
+ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+ cityElement.innerHTML = response.data.city;
+ descriptionElement.innerHTML = response.data.condition.description;
  temperatureElement.innerHTML = Math.round(temperature);         
+
+}
+function formatDate(date){
+ 
+ let minutes = date.getMinutes();
+ let hours = date.getHours();
+ let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+ let day = days[date.getDay()];
+ if (minutes < 10){
+     minutes = `0${minutes}`;
+ }
+
+ return `${day} ${hours}:${minutes}`;
 
 }
 function findCity(city){
